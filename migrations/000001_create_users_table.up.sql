21753c4d0b85822ca413bac18893105b8196e86e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 00:49:28 +0700

    refactor: adjust schema table users

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE "users" (
     "id" serial PRIMARY KEY,
     "fullName" varchar(255) NOT NULL,
     "email" varchar(255) UNIQUE NOT NULL,
-    "role" varchar(20) NOT NULL DEFAULT 'customer',
     "password" text NOT NULL,
     "created_at" timestamp DEFAULT (CURRENT_TIMESTAMP),
     "updated_at" timestamp DEFAULT (CURRENT_TIMESTAMP),

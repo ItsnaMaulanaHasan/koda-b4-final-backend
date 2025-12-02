a07361856689fbc08e8ef3430b88d1dcdf9f2916
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 16:53:15 +0700

    feat: adjust route

--- internal/routes/main.go
+++ internal/routes/main.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"backend-koda-shortlink/internal/database"
 	"backend-koda-shortlink/internal/handlers"
+	"backend-koda-shortlink/internal/middlewares"
 	"backend-koda-shortlink/internal/repository"
 	"backend-koda-shortlink/internal/services"
 
@@ -9,14 +11,18 @@ import (
 )
 
 func SetUpRoutes(r *gin.Engine) {
-	userRepo := repository.NewUserRepository()
-	sessionRepo := repository.NewSessionRepository()
+	userRepo := repository.NewUserRepository(database.DB)
+	sessionRepo := repository.NewSessionRepository(database.DB)
+	shortLinkRepo := repository.NewShortLinkRepository(database.DB)
 
 	authService := services.NewAuthService(userRepo, sessionRepo)
+	shortLinkService := services.NewShortLinkService(shortLinkRepo)
 
 	authHandler := handlers.NewAuthHandler(authService)
+	shortLinkHandler := handlers.NewShortLinkHandler(shortLinkService)
 
-	// authMiddleware := middlewares.NewAuthMiddleware(sessionRepo)
+	authMiddleware := middlewares.NewAuthMiddleware(*sessionRepo)
 
 	authRouter(r.Group("/api/v1/auth"), authHandler)
+	shortLinkRoutes(r.Group("/api/v1/links", authMiddleware.Auth()), shortLinkHandler)
 }

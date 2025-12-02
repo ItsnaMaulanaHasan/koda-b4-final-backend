20b4e9c0143f4cca30d510715f240e1c466ea56e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 14:33:35 +0700

    feat: adjust router

--- internal/routes/auth.router.go
+++ internal/routes/auth.router.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	"backend-koda-shortlink/internal/controllers"
+	"backend-koda-shortlink/internal/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
-func authRouter(r *gin.RouterGroup) {
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
-	r.POST("/refresh", controllers.RefreshToken)
-	r.POST("/logout", controllers.Logout)
+func authRouter(r *gin.RouterGroup, authHandler *handlers.AuthHandler) {
+	r.POST("/register", authHandler.Register)
+	r.POST("/login", authHandler.Login)
+	r.POST("/refresh", authHandler.RefreshToken)
+	r.POST("/logout", authHandler.Logout)
 }
--- internal/routes/main.go
+++ internal/routes/main.go
@@ -1,9 +1,22 @@
 package routes
 
 import (
+	"backend-koda-shortlink/internal/handlers"
+	"backend-koda-shortlink/internal/repository"
+	"backend-koda-shortlink/internal/services"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes(r *gin.Engine) {
-	authRouter(r.Group("/api/v1/auth"))
+	userRepo := repository.NewUserRepository()
+	sessionRepo := repository.NewSessionRepository()
+
+	authService := services.NewAuthService(userRepo, sessionRepo)
+
+	authHandler := handlers.NewAuthHandler(authService)
+
+	// authMiddleware := middlewares.NewAuthMiddleware(sessionRepo)
+
+	authRouter(r.Group("/api/v1/auth"), authHandler)
 }

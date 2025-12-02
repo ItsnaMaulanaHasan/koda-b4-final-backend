b3821fd0ab1998a214c601d96285ac70ef769b77
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 20:03:32 +0700

    feat: create route for dashboard stats

--- internal/routes/main.go
+++ internal/routes/main.go
@@ -14,17 +14,23 @@ func SetUpRoutes(r *gin.Engine) {
 	userRepo := repository.NewUserRepository(database.DB)
 	sessionRepo := repository.NewSessionRepository(database.DB)
 	shortLinkRepo := repository.NewShortLinkRepository(database.DB)
+	clickRepo := repository.NewClickRepository(database.DB)
+	dashboardRepo := repository.NewDashboardRepository(database.DB)
 
 	authService := services.NewAuthService(userRepo, sessionRepo)
-	shortLinkService := services.NewShortLinkService(shortLinkRepo)
+	shortLinkService := services.NewShortLinkService(shortLinkRepo, clickRepo)
+	dashboardService := services.NewDashboardService(dashboardRepo)
 
 	authHandler := handlers.NewAuthHandler(authService)
 	shortLinkHandler := handlers.NewShortLinkHandler(shortLinkService)
+	dashboardHandler := handlers.NewDashboardHandler(dashboardService)
 
 	authMiddleware := middlewares.NewAuthMiddleware(*sessionRepo)
 
 	authRouter(r.Group("/api/v1/auth"), authHandler)
 	shortLinkRoutes(r.Group("/api/v1/links", authMiddleware.Auth()), shortLinkHandler)
+
 	r.GET("/:shortCode", shortLinkHandler.Redirect)
 
+	r.GET("/api/v1/dashboard/stats", authMiddleware.Auth(), dashboardHandler.Stats)
 }

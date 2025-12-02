e610949ac94e601c66655544917d9da7fe0f130b
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 21:39:01 +0700

    fix: user model

--- internal/models/auth.model.go
+++ internal/models/auth.model.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
-	Id       int    `json:"id" db:"id"`
-	FullName string `json:"fullName" db:"fullname"`
-	Email    string `json:"email" db:"email"`
-	Password string `json:"-" db:"password"`
+	Id           int     `json:"id" db:"id"`
+	ProfilePhoto *string `json:"profilePhoto" db:"profile_photo"`
+	FullName     string  `json:"fullName" db:"fullname"`
+	Email        string  `json:"email" db:"email"`
+	Password     string  `json:"-" db:"-"`
 }
 
 type RegisterRequest struct {

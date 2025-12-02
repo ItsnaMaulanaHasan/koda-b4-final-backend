fe3f17059342f9c3d1d320ba628046a7d96e337a
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-12-02 21:00:49 +0700

    feat: create middleware for optional auth

---
+++ internal/middlewares/optional_auth.middleware.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"backend-koda-shortlink/internal/repository"
+	"backend-koda-shortlink/internal/utils"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OptionalAuthMiddleware struct {
+	sessionRepo repository.SessionRepository
+}
+
+func NewOptionalAuthMiddleware(sessionRepo repository.SessionRepository) *OptionalAuthMiddleware {
+	return &OptionalAuthMiddleware{
+		sessionRepo: sessionRepo,
+	}
+}
+
+func (m *OptionalAuthMiddleware) OptionalAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.Request.Header.Get("Authorization")
+
+		tokenString, found := strings.CutPrefix(authHeader, "Bearer ")
+		if !found || tokenString == "" {
+			ctx.Next()
+			return
+		}
+
+		claims, err := utils.VerifyAccessToken(tokenString)
+		if err != nil {
+			ctx.Next()
+			return
+		}
+
+		isActive, err := m.sessionRepo.CheckActive(ctx.Request.Context(), claims.SessionId)
+		if err != nil || !isActive {
+			ctx.Next()
+			return
+		}
+
+		ctx.Set("userId", claims.Id)
+		ctx.Set("sessionId", claims.SessionId)
+
+		ctx.Next()
+	}
+}
